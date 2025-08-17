dd24512b286ee27c02517675125461c1b6b268eb
WAYNEisBATMAN <p.jangraaa@gmail.com>
2025-08-18 03:52:16 +0530

    adding ingress manifests for vote and result svc

--- vote/app.py
+++ vote/app.py
@@ -1,3 +1,67 @@
+# from flask import Flask, render_template, request, make_response, g
+# from redis import Redis
+# import os
+# import socket
+# import random
+# import json
+# import logging
+
+# option_a = os.getenv('OPTION_A', "Cats")
+# option_b = os.getenv('OPTION_B', "Dogs")
+# hostname = socket.gethostname()
+
+# # app = Flask(__name__)
+
+# app = Flask(
+#     __name__,
+#     static_url_path='/vote/static',  # match Ingress path
+#     static_folder='static'
+# )
+
+
+# gunicorn_error_logger = logging.getLogger('gunicorn.error')
+# app.logger.handlers.extend(gunicorn_error_logger.handlers)
+# app.logger.setLevel(logging.INFO)
+
+# def get_redis():
+#     if not hasattr(g, 'redis'):
+#         g.redis = Redis(host="redis", db=0, socket_timeout=5)
+#     return g.redis
+
+# @app.route("/vote", methods=['POST','GET'])  # Add this line
+# @app.route("/", methods=['POST','GET'])
+# def hello():
+#     voter_id = request.cookies.get('voter_id')
+#     if not voter_id:
+#         voter_id = hex(random.getrandbits(64))[2:-1]
+
+#     vote = None
+
+#     if request.method == 'POST':
+#         redis = get_redis()
+#         vote = request.form['vote']
+#         app.logger.info('Received vote for %s', vote)
+#         data = json.dumps({'voter_id': voter_id, 'vote': vote})
+#         redis.rpush('votes', data)
+
+#     resp = make_response(render_template(
+#         'index.html',
+#         option_a=option_a,
+#         option_b=option_b,
+#         hostname=hostname,
+#         vote=vote,
+#     ))
+
+#     resp.headers['Content-Type'] = 'text/html; charset=utf-8'
+#     resp.set_cookie('voter_id', voter_id)
+#     return resp
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+
+
+
 from flask import Flask, render_template, request, make_response, g
 from redis import Redis
 import os
@@ -10,14 +74,7 @@ option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
-# app = Flask(__name__)
-
-app = Flask(
-    __name__,
-    static_url_path='/vote/static',  # match Ingress path
-    static_folder='static'
-)
-
+app = Flask(__name__)
 
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
@@ -28,7 +85,6 @@ def get_redis():
         g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
 
-@app.route("/vote", methods=['POST','GET'])  # Add this line
 @app.route("/", methods=['POST','GET'])
 def hello():
     voter_id = request.cookies.get('voter_id')
